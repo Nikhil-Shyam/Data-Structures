e52c0d3f85b5498ded371e35720a956f36462442
Nikhil-Shyam <115193450+Nikhil-Shyam@users.noreply.github.com>
2023-09-14 19:29:48 -0400

    Create Template.java

---
+++ Template.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Template {
+    public Template(){
+        try{
+            File file = new File("CubeInput.txt");
+            BufferedReader input = new BufferedReader(new FileReader(file));
+
+            String st;
+            while ((st = input.readLine()) != null){
+                
+            }
+
+        }        
+        catch(IOException e){
+
+        }
+    }
+    public static void main(String[] args){
+        Template app = new Template();
+    }
+}
