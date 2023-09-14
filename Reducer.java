0493f1cd2733d89cb14e0c62399d38f476e10fac
Nikhil-Shyam <115193450+Nikhil-Shyam@users.noreply.github.com>
2023-09-14 08:46:19 -0400

    Update Reducer.java

--- Reducer.java
+++ Reducer.java
@@ -16,22 +16,40 @@ public class Template {
                 int den = Integer.parseInt(pieces[1]);
                 // loop through to find the gcd and reduce on the way
                 // display your answer
+
+                // special case where the numerator is 0
                 if (num == 0)
                     System.out.print(0);
+
+                // special case where the denominator is 0
                 if (den == 0)
                     System.out.print("undef");
+
+                // case where numerator is bigger than denominator
+                // i then print out whatever num/den is because it'll be an integer
+                // then do num % den to get the remainder which makes it the case where numerator is smaller than denominator
                 if (num > den && den != 0){
                     System.out.print((num/den) + " ");
                     num %= den;
                 }
+
+                // greatest common divisor
                 int gcd = 2;
+
+                // running this loop as long as my gcd is less than my numerator
                 while(gcd <= num && num != 0 && den != 0){
+
+                    // if my numerator and denominator are divisible by the gcd i go ahead and divide both
                     if (num % gcd == 0 && den % gcd == 0){
                         num /= gcd;
                         den /= gcd;
                     }
+
+                    // then i move to the next gcd
                     gcd++;
                 }
+
+                // printing
                 if (num == den)
                     System.out.println(1);
                 else if (num == 0 || den == 0)
